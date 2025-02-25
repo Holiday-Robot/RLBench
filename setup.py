747b701adf15f7e87c04803055ca7d0991b5da78
jaemin.yoon <jaemin.yoon@holiday-robotics.com>
2025-02-25 16:43:05 +0900

    make easy task for insertion

--- setup.py
+++ setup.py
@@ -11,17 +11,18 @@ from setuptools import setup, find_packages
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
+
 
 core_requirements = [
     "pyrep @ git+https://github.com/stepjam/PyRep.git",
@@ -29,27 +30,25 @@ core_requirements = [
     "Pillow",
     "pyquaternion",
     "scipy",
-    "natsort"
+    "natsort",
 ]
 
-setup(name='rlbench',
-      version=get_version("rlbench/__init__.py"),
-      description='RLBench',
-      author='Stephen James',
-      author_email='slj12@ic.ac.uk',
-      url='https://www.doc.ic.ac.uk/~slj12',
-      install_requires=core_requirements,
-      packages=find_packages(),
-      include_package_data=True,
-      extras_require={
-        "gym": ["gymnasium==1.0.0a2"],
-        "dev": ["pytest"]
-      },
-      package_data={'': ['*.ttm', '*.obj', '**/**/*.ttm', '**/**/*.obj'],
-                    'rlbench': ['task_design.ttt']},
-      entry_points={
-          "console_scripts": [
-              "rlbench-generate-dataset = rlbench.dataset_generator:main"
-          ]
-      }
+setup(
+    name="rlbench",
+    version=get_version("rlbench/__init__.py"),
+    description="RLBench",
+    author="Stephen James",
+    author_email="slj12@ic.ac.uk",
+    url="https://www.doc.ic.ac.uk/~slj12",
+    install_requires=core_requirements,
+    packages=find_packages(),
+    include_package_data=True,
+    extras_require={"gym": ["gymnasium==1.0.0a2"], "dev": ["pytest"]},
+    package_data={
+        "": ["*.ttm", "*.obj", "**/**/*.ttm", "**/**/*.obj"],
+        "rlbench": ["task_design.ttt"],
+    },
+    entry_points={
+        "console_scripts": ["rlbench-generate-dataset = rlbench.dataset_generator:main"]
+    },
 )
